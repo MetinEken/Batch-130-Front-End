0bb64e7d248763c7ba952cd6cde41602fa3a683f
MetinEken <metineken2021@gmail.com>
2023-09-09 15:22:53 +0200

    last codes

---
+++ React-Instructure/basics/src/App.js
@@ -0,0 +1,34 @@
+import X from "./components/01-my-house/01-my-house";
+import { Armutlar, Muzlar } from "./components/01-my-house/01-my-house";
+import Jsx1 from "./components/02-jsx/01-jsx";
+import Jsx2 from "./components/02-jsx/02-jsx";
+import Jsx3 from "./components/02-jsx/03-jsx";
+import Jsx4 from "./components/02-jsx/04-jsx";
+import InlineStyling from "./components/03-styles/01-inline-styling"
+import InternalStyling from "./components/03-styles/02-internal-styling";
+ import ExternalStyling from "./components/03-styles/03-external-styling"
+ import ModuleStyling from "./components/03-styles/04-module-styling"
+import Sass from "./components/03-styles/05-sass";
+import DijitalSaatUygulamasi1 from "./components/04-dijital-saat-uygulamasi1/04-dijital-saat-uygulamasi1";
+
+const App = () => {
+    return (
+        <div>
+            <X />
+            <Armutlar />
+            <Muzlar />
+            <Jsx1 />
+            <Jsx2 />
+            <Jsx3 />
+            <Jsx4 />
+            <InlineStyling />
+           <InternalStyling />
+           <ExternalStyling />
+           <ModuleStyling />
+           <Sass />
+           <DijitalSaatUygulamasi1 />
+        </div>
+    )
+};
+
+export default App;
---
+++ React-Practice/myapp/src/App.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import SelectedCourses from './components/c4/SelectedCourses'
+/* import Filtered from './components/c3/Filtered' */
+/* import IsLogin from './components/c2/IsLogin' */
+/* import Dersler from './components/c1/Dersler' */
+
+const App = () => {
+  return (
+    <div>
+    {/* 
+    
+<Dersler/> 
+<IsLogin/>    
+<Filtered/>    
+    
+    
+    
+    
+    */}
+
+    <SelectedCourses/>
+
+
+
+
+
+
+
+
+    </div>
+  )
+}
+
+export default App
